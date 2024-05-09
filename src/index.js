ce1e35bcf20c853414315a16ffa7ff4b1b3512b2
Vallieire <Kareem_adel9686@yahoo.com>
2024-05-09 23:33:12 +0300

    aws prof update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-//const mongoose = require('mongoose');
 const redis = require('redis');
-const { Client } = require('pg');
+// const mongoose = require('mongoose'); // Commented out Mongoose
+// const { Client } = require('pg'); // Import PostgreSQL Client
 
 // Initialize app
 const PORT = 4000;
@@ -15,12 +15,11 @@ const redisclient = redis.createClient({
 });
 redisclient.on('error', err => console.log('Redis Client Error', err));
 redisclient.on('connect', () => console.log('Connected to Redis....'));
-redisclient.connect();
 
 // Define route for setting Redis value
 app.get('/', (req, res) => {
   redisclient.set('products', 'products');
-  res.send('<h1>Hello kemo! basha aws prof</h1>');
+  res.send('<h1>Hello kemo! basha aws prof super prof15000</h1>');
 });
 
 // Define route for getting Redis value
@@ -29,38 +28,21 @@ app.get('/data', async (req, res) => {
   res.send(`<h1>Hello kemo!</h1><h2>${products}</h2>`);
 });
 
-
-//  Connect to Postgres
- 
-const DB_USER = 'root';
-const DB_PASSWORD = 'example';
-const DB_PORT = '5432';
-const DB_HOST = 'postgres'; 
-
-const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-const client = new Client({
-  connectionString: URI,
-});
-
-client
-  .connect()
-  .then(() => console.log('Connected to PostgresDB'))
-  .catch((err) => console.log(`Failed to connect to PostgresDB: ${err}`));
-
-
-
-// Connect to MongoDB
+// Connect to PostgreSQL (Commented out)
 // const DB_USER = 'root';
 // const DB_PASSWORD = 'example';
-// const DB_PORT = '27017';
-// const DB_HOST = 'mongo'; 
+// const DB_PORT = '5432';
+// const DB_HOST = 'postgres'; 
 
-// const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-// mongoose.connect(URI)
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch((err) => console.log(`Failed to connect to MongoDB: ${err}`));
+// const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+// const client = new Client({
+//   connectionString: URI,
+// });
 
-
+// client
+//   .connect()
+//   .then(() => console.log('Connected to PostgreSQL'))
+//   .catch((err) => console.log(`Failed to connect to PostgreSQL: ${err}`));
 
 // Start server
 app.listen(PORT, () => console.log(`App is up and running on port: ${PORT}`));
